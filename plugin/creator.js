df694960f49f53bfda7a0d0ebe6e52ef903e36be
Xiaomi Team <112295130+XiaomiBOT@users.noreply.github.com>
2022-09-14 20:58:15 +0700

    Update and rename plugins/creator.js to plugin/creator.js

--- plugin/creator.js
+++ plugin/creator.js
@@ -20,16 +20,16 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = 'https://telegra.ph/file/cce9ab4551f7150f1970d.jpg'
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : Yan Xiao
+${htjava} *💌 Nama* : Rangga Official
 ${htjava} *✉️ Nama RL* : Rahasia🗿
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
-${htjava} *⏰ Tanggal lahir* : 15 Oktober 2004
-${htjava} *🎨 Umur* : 17
-${htjava} *🧮 Kelas* : 12
+${htjava} *⏰ Tanggal lahir* : 7 April 2009
+${htjava} *🎨 Umur* : 13
+${htjava} *🧮 Kelas* : 7
 ${htjava} *🧩 Hobby* : Nonton Donghua, Chatting, Musik, Recode script bot
 ${htjava} *💬 Sifat* : Idiot, Tidak Ramah, Bilek, Prik, Nolep
-${htjava} *🗺️ Tinggal* : Indo, Tulungagung, Jawa Timur
+${htjava} *🗺️ Tinggal* : Indo, Bekasi, Jawa Barat
 ${htjava} *❤️ Suka* : kucing
 ${htjava} *💔 Benci* : autis, seleb
 
